5b0a7dc65b48cf35d210ad52ab2aac7bf442915b
Maksym Menzheha <masimfyd@gmail.com>
2022-04-19 16:45:56 +0300

    update hero entity
    add create and update date

--- src/heroes/hero.entity.ts
+++ src/heroes/hero.entity.ts
@@ -1,4 +1,10 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 @Entity()
 export class Hero {
@@ -21,4 +27,10 @@ export class Hero {
   catch_phrase: string;
 
   //images
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
