1a1d6c9e207c7b6cda04dc0be5bd965869c69afa
Maksym Menzheha <masimfyd@gmail.com>
2022-04-21 19:33:52 +0300

    extend file limit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
+import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+  app.enableCors();
   await app.listen(4000);
 }
 bootstrap();
