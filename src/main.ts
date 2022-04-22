e531f6ddc06e32409606b0857521b8e8f989c2ff
Maksym Menzheha <masimfyd@gmail.com>
2022-04-22 13:59:55 +0300

    add allowed methods to cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { json, urlencoded } from 'express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
   app.use(json({ limit: '50mb' }));
   app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(4000);
